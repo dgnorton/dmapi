6093f5eb1292e629f5ffd616ee480ab42047e518
David Norton <david@influxdb.com>
2014-09-15 16:45:10 -0400

    Add workoutFelt parameter to Find & go fmt

--- client.go
+++ client.go
@@ -1,199 +1,199 @@
 package dmapi
 
 import (
-   "encoding/json"
-   "fmt"
-   "io/ioutil"
-   "net/http"
-   "strconv"
-   "time"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"time"
 )
 
 type Entry struct {
-   Id int `json:"id"`
-   Url string `json:"url"`
-   At string `json:"at"`
-   Message string `json:"message"`
-   Comments []Comment `json:"comments"`
-   Likes []Like `json:"likes"`
-   Geo Geo `json:"geo"`
-   Location Location `json:"location"`
-   User User `json:"user"`
-   Workout Workout `json:"workout"`
+	Id       int       `json:"id"`
+	Url      string    `json:"url"`
+	At       string    `json:"at"`
+	Message  string    `json:"message"`
+	Comments []Comment `json:"comments"`
+	Likes    []Like    `json:"likes"`
+	Geo      Geo       `json:"geo"`
+	Location Location  `json:"location"`
+	User     User      `json:"user"`
+	Workout  Workout   `json:"workout"`
 }
 
 func (e *Entry) Time() (time.Time, error) {
-   return time.Parse(time.RFC3339, e.At)
+	return time.Parse(time.RFC3339, e.At)
 }
 
 type Comment struct {
-   Body string `json:"body"`
-   CreatedAt string `json:"created_at"`
-   User User `json:"user"`
+	Body      string `json:"body"`
+	CreatedAt string `json:"created_at"`
+	User      User   `json:"user"`
 }
 
 type Like struct {
 }
 
 type Location struct {
-   Name string `json:"name"`
+	Name string `json:"name"`
 }
 
 type User struct {
-   Name string `json:"username"`
-   DisplayName string `json:"display_name"`
-   PhotoUrl string `json:"photo_url"`
-   Url string `json:"usr"`
+	Name        string `json:"username"`
+	DisplayName string `json:"display_name"`
+	PhotoUrl    string `json:"photo_url"`
+	Url         string `json:"usr"`
 }
 
 type Workout struct {
-   Type string `json:"activity_type"`
-   Distance Distance `json:"distance"`
-   Felt string `json:"felt"`
-   Dur int `json:"duration"`
-   Title string `json:"title"`
+	Type     string   `json:"activity_type"`
+	Distance Distance `json:"distance"`
+	Felt     string   `json:"felt"`
+	Dur      int      `json:"duration"`
+	Title    string   `json:"title"`
 }
 
 func (w Workout) Pace() (time.Duration, error) {
-   if w.Distance.Value <= 0.0 {
-      return time.Duration(0.0), nil
-   }
-   secPerUnit := w.Duration().Seconds() / w.Distance.Value
-   return time.ParseDuration(strconv.FormatFloat(secPerUnit, 'f', 6, 64) + "s")
+	if w.Distance.Value <= 0.0 {
+		return time.Duration(0.0), nil
+	}
+	secPerUnit := w.Duration().Seconds() / w.Distance.Value
+	return time.ParseDuration(strconv.FormatFloat(secPerUnit, 'f', 6, 64) + "s")
 }
 
 func (w Workout) PaceStr() (string, error) {
-   d, err := w.Pace()
-   if err != nil {
-      return "", err
-   }
-   return DurationStr(d), nil
+	d, err := w.Pace()
+	if err != nil {
+		return "", err
+	}
+	return DurationStr(d), nil
 }
 
 func (w Workout) Duration() time.Duration {
-   dur, _ := time.ParseDuration(strconv.Itoa(w.Dur) + "s")
-   return dur
+	dur, _ := time.ParseDuration(strconv.Itoa(w.Dur) + "s")
+	return dur
 }
 
 func DurationStr(d time.Duration) string {
-   totSec := int(d.Seconds())
-   h := totSec / 3600
-   m := (totSec - (h * 3600)) / 60
-   s := totSec - (h * 3600) - (m * 60)
+	totSec := int(d.Seconds())
+	h := totSec / 3600
+	m := (totSec - (h * 3600)) / 60
+	s := totSec - (h * 3600) - (m * 60)
 
-   if h > 0 {
-      return fmt.Sprintf("%d:%02d:%02d", h, m, s)
-   }
+	if h > 0 {
+		return fmt.Sprintf("%d:%02d:%02d", h, m, s)
+	}
 
-   return fmt.Sprintf("%d:%02d", m, s)
+	return fmt.Sprintf("%d:%02d", m, s)
 }
 
 func (w Workout) DurationStrColons() (string, error) {
-   d, err := time.ParseDuration(strconv.Itoa(w.Dur) + "s")
-   if err != nil {
-      return "", err
-   }
-   return DurationStr(d), nil
+	d, err := time.ParseDuration(strconv.Itoa(w.Dur) + "s")
+	if err != nil {
+		return "", err
+	}
+	return DurationStr(d), nil
 }
 
 type Distance struct {
-   Value float64 `json:"value"`
-   Units string `json:"units"`
+	Value float64 `json:"value"`
+	Units string  `json:"units"`
 }
 
 type Geo struct {
-   Type string `json:"type"`
-   // longitude, latitude ... in that order
-   Coordinates []string `json:"coordinates"`
+	Type string `json:"type"`
+	// longitude, latitude ... in that order
+	Coordinates []string `json:"coordinates"`
 }
 
 type Entries struct {
-   Entries []Entry `json:"entries"`
+	Entries []Entry `json:"entries"`
 }
 
 func (e Entries) TotalDistance() float64 {
-   var d float64 = 0.0
-   for _, entry := range e.Entries {
-      // need to fix to deal with different units
-      d += entry.Workout.Distance.Value
-   }
-   return d
+	var d float64 = 0.0
+	for _, entry := range e.Entries {
+		// need to fix to deal with different units
+		d += entry.Workout.Distance.Value
+	}
+	return d
 }
 
 func (e Entries) AvgPace() (time.Duration, error) {
-   var avg int64 = 0
-   var cnt int64 = 0
+	var avg int64 = 0
+	var cnt int64 = 0
 
-   for _, entry := range e.Entries {
-      p, err := entry.Workout.Pace()
-      if entry.Workout.Distance.Units == "kilometers" {
-         p = mpkToMpm(p)
-      }
-      if err != nil {
-         return time.Duration(0), err
-      }
+	for _, entry := range e.Entries {
+		p, err := entry.Workout.Pace()
+		if entry.Workout.Distance.Units == "kilometers" {
+			p = mpkToMpm(p)
+		}
+		if err != nil {
+			return time.Duration(0), err
+		}
 
-      avg += p.Nanoseconds()
-      if p.Nanoseconds() > 0 {
-         cnt++
-      }
-   }
+		avg += p.Nanoseconds()
+		if p.Nanoseconds() > 0 {
+			cnt++
+		}
+	}
 
-   if cnt > 0 {
-      avg /= cnt
-   }
+	if cnt > 0 {
+		avg /= cnt
+	}
 
-   return time.Duration(avg), nil
+	return time.Duration(avg), nil
 }
 
 func (e Entries) AvgPaceStr() string {
-   d, err := e.AvgPace()
-   if err != nil {
-      return ""
-   }
-   return DurationStr(d)
+	d, err := e.AvgPace()
+	if err != nil {
+		return ""
+	}
+	return DurationStr(d)
 }
 
 // Converts minutes/km to minutes/mi
 func mpkToMpm(d time.Duration) time.Duration {
-   return time.Duration(float64(d) * 1.609)
+	return time.Duration(float64(d) * 1.609)
 }
 
 func EntriesByPage(user string, page int) (*Entries, error) {
-   req := fmt.Sprintf("http://api.dailymile.com/people/%s/entries.json?page=%d", user, page)
-   resp, err := http.Get(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   body, err := ioutil.ReadAll(resp.Body)
-   if err != nil {
-      return nil, err
-   }
+	req := fmt.Sprintf("http://api.dailymile.com/people/%s/entries.json?page=%d", user, page)
+	resp, err := http.Get(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
 
-   var entries Entries
-   err = json.Unmarshal(body, &entries)
+	var entries Entries
+	err = json.Unmarshal(body, &entries)
 
-   return &entries, err
+	return &entries, err
 }
 
 func EntriesSince(user string, unixTime int64) (*Entries, error) {
-   req := fmt.Sprintf("http://api.dailymile.com/people/%s/entries.json?since=%d", user, unixTime)
-   resp, err := http.Get(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   body, err := ioutil.ReadAll(resp.Body)
-   if err != nil {
-      return nil, err
-   }
+	req := fmt.Sprintf("http://api.dailymile.com/people/%s/entries.json?since=%d", user, unixTime)
+	resp, err := http.Get(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
 
-   var entries Entries
+	var entries Entries
 
-   if len(body) > 0 {
-      err = json.Unmarshal(body, &entries)
-   }
+	if len(body) > 0 {
+		err = json.Unmarshal(body, &entries)
+	}
 
-   return &entries, err
+	return &entries, err
 }
--- data.go
+++ data.go
@@ -1,125 +1,133 @@
 package dmapi
 
 import (
-   "encoding/json"
-   "errors"
-   "io/ioutil"
-   "regexp"
-   "time"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"regexp"
+	"time"
 )
 
 func SaveEntries(file string, entries *Entries) error {
-   bytes, err := json.Marshal(entries)
-   if err != nil {
-      return err
-   }
-   err = ioutil.WriteFile(file, bytes, 0600)
-   return err
+	bytes, err := json.Marshal(entries)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(file, bytes, 0600)
+	return err
 }
 
 func LoadEntries(file string) (*Entries, error) {
-   bytes, err := ioutil.ReadFile(file)
-   if err != nil {
-      return nil, err
-   }
-   var entries Entries
-   err = json.Unmarshal(bytes, &entries)
-   return &entries, err
+	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	var entries Entries
+	err = json.Unmarshal(bytes, &entries)
+	return &entries, err
 }
 
 func (e *Entries) Remove(id int) error {
-   for i, entry := range e.Entries {
-      if entry.Id == id || id == -1 {
-         e.Entries = append(e.Entries[:i], e.Entries[i+1:]...)
-         return nil
-      }
-   }
-   return errors.New("id not found")
+	for i, entry := range e.Entries {
+		if entry.Id == id || id == -1 {
+			e.Entries = append(e.Entries[:i], e.Entries[i+1:]...)
+			return nil
+		}
+	}
+	return errors.New("id not found")
 }
 
-func (e *Entries) Find(startDate, endDate, pattern, workoutType string) (*Entries, error) {
-   var start, end time.Time
-   loc := time.Now().Location()
+func (e *Entries) Find(startDate, endDate, pattern, workoutType string, workoutFelt string) (*Entries, error) {
+	var start, end time.Time
+	loc := time.Now().Location()
 
-   if startDate != "" {
-      template, err := dateTemplate(startDate)
-      if err != nil {
-         return nil, err
-      }
-      start, err = time.ParseInLocation(template, startDate, loc)
-      if err != nil {
-         return nil, err
-      }
-   }
+	if startDate != "" {
+		template, err := dateTemplate(startDate)
+		if err != nil {
+			return nil, err
+		}
+		start, err = time.ParseInLocation(template, startDate, loc)
+		if err != nil {
+			return nil, err
+		}
+	}
 
-   if startDate != "" && endDate != "" {
-      template, err := dateTemplate(endDate)
-      if err != nil {
-         return nil, err
-      }
-      end, err = time.ParseInLocation(template, endDate, loc)
-      if err != nil {
-         return nil, err
-      }
-      end = time.Date(end.Year(), end.Month(), end.Day(), 23, 59, 59, 0, end.Location())
-   }
+	if startDate != "" && endDate != "" {
+		template, err := dateTemplate(endDate)
+		if err != nil {
+			return nil, err
+		}
+		end, err = time.ParseInLocation(template, endDate, loc)
+		if err != nil {
+			return nil, err
+		}
+		end = time.Date(end.Year(), end.Month(), end.Day(), 23, 59, 59, 0, end.Location())
+	}
 
-   if pattern == "*" || pattern == "" {
-      pattern = ".*"
-   }
+	if pattern == "*" || pattern == "" {
+		pattern = ".*"
+	}
 
-   regex, err := regexp.Compile(pattern)
-   if err != nil {
-      return nil, err
-   }
+	regex, err := regexp.Compile(pattern)
+	if err != nil {
+		return nil, err
+	}
 
-   useType := false
-   if (len(workoutType) > 0) {
-      useType = true
-   }
+	useType := false
+	if len(workoutType) > 0 {
+		useType = true
+	}
 
-   var matches Entries
-   for _, entry := range e.Entries {
-      if entry.Workout.Type == "" {
-         continue
-      }
-      if useType &&  entry.Workout.Type != workoutType {
-         continue
-      }
-      if startDate != "" {
-         t, err := entry.Time()
-         if err != nil {
-            continue
-         }
-         t = t.Local()
-         if t.Before(start) {
-            continue
-         } else if endDate != "" && t.After(end) {
-            continue
-         }
-      }
-      if regex.MatchString(entry.Workout.Title) || regex.MatchString(entry.Message) {
-         matches.Entries = append(matches.Entries, entry)
-      }
-   }
-   return &matches, nil
+	useFelt := false
+	if len(workoutFelt) > 0 {
+		useFelt = true
+	}
+
+	var matches Entries
+	for _, entry := range e.Entries {
+		if entry.Workout.Type == "" {
+			continue
+		}
+		if useType && entry.Workout.Type != workoutType {
+			continue
+		}
+		if useFelt && entry.Workout.Felt != workoutFelt {
+			continue
+		}
+		if startDate != "" {
+			t, err := entry.Time()
+			if err != nil {
+				continue
+			}
+			t = t.Local()
+			if t.Before(start) {
+				continue
+			} else if endDate != "" && t.After(end) {
+				continue
+			}
+		}
+		if regex.MatchString(entry.Workout.Title) || regex.MatchString(entry.Message) {
+			matches.Entries = append(matches.Entries, entry)
+		}
+	}
+	return &matches, nil
 }
 
 type dateTemplateTest struct {
-   Regex *regexp.Regexp
-   Template string
+	Regex    *regexp.Regexp
+	Template string
 }
 
-var dateTemplates = []dateTemplateTest {
-   { regexp.MustCompile(`^\d{2}/\d{1,2}/\d{1,2}$`), "06/1/2" },
-   { regexp.MustCompile(`^\d{2]/\d{1,2}$`), "06/1" },
+var dateTemplates = []dateTemplateTest{
+	{regexp.MustCompile(`^\d{2}/\d{1,2}/\d{1,2}$`), "06/1/2"},
+	{regexp.MustCompile(`^\d{2]/\d{1,2}$`), "06/1"},
 }
 
 func dateTemplate(date string) (string, error) {
-   for _, tst := range dateTemplates {
-      if tst.Regex.MatchString(date) {
-         return tst.Template, nil
-      }
-   }
-   return "", errors.New("Invalid date format")
+	for _, tst := range dateTemplates {
+		if tst.Regex.MatchString(date) {
+			return tst.Template, nil
+		}
+	}
+	return "", errors.New("Invalid date format")
 }
